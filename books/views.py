421621ee3c718c38efe207afcc74cc74fff8ae2c
TrapKingg <a_niupi_byy@hotmail.com>
2017-04-06 10:02:15 -0500

    Se añadio el sistema de cambio de contraseñas

--- books/views.py
+++ books/views.py
@@ -6,6 +6,5 @@ from .models import Book
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required
 def index(request):
     return render(request, 'index.html', {})
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from .views import LoginView
-from django.contrib.auth.views import logout,logout_then_login
+from django.contrib.auth.views import logout,logout_then_login, password_change, password_change_done
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+    #Change password urls
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change-done/$', password_change_done, name='password_change_done'),
+
 ]
